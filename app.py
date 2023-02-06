e93732ad513c7649dcc5a810d69c6615d5993564
layz1008 <zlay1008@gmail.com>
2023-02-06 10:56:57 -0500

    chicken

--- app.py
+++ app.py
@@ -21,6 +21,30 @@ Chicken(name='Gertie', breed='Barred Plymouth Rock').save()
 
 app = Flask(__name__)
 
+@app.route('/chicken/', methods=['GET', 'POST'])
+@app.route('/chicken/<id>', methods=['GET', 'PUT', 'DELETE'])
+def endpoint(id=None):
+  if request.method == 'GET':
+    if id:
+        return jsonify(model_to_dict(Chicken.get(Chicken.id == id)))
+    else:
+        chickens_list = []
+        for chicken in Chicken.select():
+            chickens_list.append(model_to_dict(chicken))
+        return jsonify(chickens_list)
 
+  if request.method =='PUT':
+    body = request.get_json()
+    Chicken.update(body).where(Chicken.id == id).execute()
+    return "Chicken " + str(id) + " has been updated."
+
+  if request.method == 'POST':
+    new_chicken = dict_to_model(Chicken, request.get_json())
+    new_chicken.save()
+    return jsonify({"success": True})
+
+  if request.method == 'DELETE':
+    Chicken.delete().where(Chicken.id == id).execute()
+    return "Chicken " + str(id) + " deleted."
 
 app.run(debug=True, port=9000)
